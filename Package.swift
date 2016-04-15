74272866996b767bbf2d2dba7e8148f4b1e4c683
Oleg Dreyman <dreymonde@me.com>
2016-04-15 10:27:02 +0300

    StructuredData dependency fix

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "MediaType",
     dependencies: [
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/InterchangeData.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/StructuredData.git", majorVersion: 0, minor: 4)
     ]
 )
