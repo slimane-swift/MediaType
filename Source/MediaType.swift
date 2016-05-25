05e0268fbb188be585992467128eabcefdc829a2
Tanner <me@tanner.xyz>
2016-05-25 13:07:17 -0400

    update to Open Swift 0.8 (#9)

--- Source/MediaType.swift
+++ Source/MediaType.swift
@@ -23,7 +23,23 @@
 // SOFTWARE.
 
 @_exported import String
-@_exported import StructuredData
+import C7
+
+public typealias StructuredData = C7.StructuredData
+
+public protocol StructuredDataParser {
+    func parse(_ data: Data) throws -> StructuredData
+}
+
+public extension StructuredDataParser {
+    public func parse(_ convertible: DataConvertible) throws -> StructuredData {
+        return try parse(convertible.data)
+    }
+}
+
+public protocol StructuredDataSerializer {
+    func serialize(_ structuredData: StructuredData) throws -> Data
+}
 
 enum MediaTypeError: ErrorProtocol {
     case malformedMediaTypeString
