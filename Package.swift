ba4a1c44731cb8946868da750406d5f5fe47b29b
noppoman <yuki@miketokyo.com>
2016-08-12 00:55:22 +0900

    sync with Quark

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "MediaType",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/StructuredData.git", majorVersion: 0, minor: 9)
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/slimane-swift/StructuredData.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/slimane-swift/Map.git", majorVersion: 0, minor: 12)
     ]
 )
--- Tests/MediaTypeTests/MediaTypeTests.swift
+++ Tests/MediaTypeTests/MediaTypeTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import MediaType
+@testable import MediaTypeTests
 
 class MediaTypeTests: XCTestCase {
     func testReality() {
